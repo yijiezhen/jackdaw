470073ef0a08fce6db27398a74d793cb02d15df1
Oliver Marshall <oliver.marshall@thetrainline.com>
2019-02-08 12:18:12 +0000

    Fix an issue where the wrong header conversion function was called

--- src/jackdaw/streams/interop.clj
+++ src/jackdaw/streams/interop.clj
@@ -77,7 +77,7 @@
 
 (defn record-context->clj
   [^RecordContext rc]
-  {:headers (header->clj (.headers rc))
+  {:headers (headers->clj (.headers rc))
    :offset (let [offset (.offset rc)]
              (when (nat-int? offset)
                offset))
